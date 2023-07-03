4f1779bd1c617b3afc586d4cb6bfc9579f767766
gabrieljdantas <gabrieljdantas26@gmail.com>
2023-07-03 13:54:32 -0300

    Operadores em JS

---
+++ aula-030723/operadores.js
@@ -0,0 +1,34 @@
+const num1 = 5;
+const num2 = 8;
+const num3 = 4.5;
+
+/*
+    Operadores aritmeticos
+
+    + adição
+    - subtração
+    * multiplicação
+    / divisão
+    **exponenciação
+
+    */
+
+    let result
+    result = num1 + num2;
+    console.log(num1 + "+" + num2 + " = " + result) ;
+
+    result = num1 - num2;
+    console.log(num1 + "-" + num2 + " = " + result)
+
+    result = num1 * num2;
+    console.log(num1 + "*" + num2 + " = " + result)
+
+    result = num1 / num2;
+    console.log(num1 + "/" + num2 + " = " + result)
+
+    result = num1 ** num2;
+    console.log(num1 + " ** " + num2 + " = " + result)
+
+    result = num1 % num2;
+    console.log(num1 + " % " + num2 + " = " + result)
+
