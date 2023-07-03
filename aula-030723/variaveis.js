0b33d8a784130b0873938341dab051394f6a4377
gabrieljdantas <gabrieljdantas26@gmail.com>
2023-07-03 12:05:06 -0300

    Aula sobre variaveis-
    030723

---
+++ aula-030723/variaveis.js
@@ -0,0 +1,32 @@
+// Java -> Linguagem de programação compilada
+// JS -> Linguagem de programação Interpretada
+
+/*
+    Multiplas linhas
+*/
+
+// Linha unica
+
+//Chrome -> Interpretador do JS
+//Node.js -> V8 + NPM (Node Package Manager)
+
+console.log("Texto")
+console.log(5)
+
+//Variaveis -> Espaço na memoria para armazenar um valor
+let nome = "Gabriel Jesus Dantas"
+console.log(nome)
+
+let altura 
+altura = 1.89
+console.log(altura)
+
+let cpf
+cpf = '471.922.638-85'
+console.log(cpf)
+
+let eCasado = false
+console.log("Casado: " + eCasado )
+
+const pi = 3.1415;
+console.log(pi)
