240af9f07e1f31dd3b2a98934a2d3da42d058e53
gabrieljdantas <gabrieljdantas26@gmail.com>
2023-07-05 09:52:15 -0300

    aula 040723

---
+++ aula-040723/atividade.js
@@ -0,0 +1,10 @@
+let num1 = 2;
+
+// Escreva um programa que verifique se um número é par ou ímpar.
+
+if (num1 % 2 == 0) {
+    console.log("par");
+}
+else {
+    console.log("impar");
+}
---
+++ aula-040723/operadores.js
@@ -0,0 +1,29 @@
+// operadores aritimeticos -> + -, *, /, %, **
+
+
+let result;
+
+result = 4 + 5; //9
+console.log(result)
+
+result = 9 * 3; //27
+console.log(result);
+
+result = 3 + 7 *12;
+console.log(result); //87
+
+result = (3 + 7) *12;
+console.log(result); //120
+
+//Operadores de Atribuição
+result += 30;
+
+//Operadores de Incremento e decremento
+
+/*
+    a++ -> pos incremento
+    ++a -> pre incremento
+
+    a-- -> pos
+    --a -< pre
+*/
