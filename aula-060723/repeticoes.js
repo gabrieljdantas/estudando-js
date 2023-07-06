f40a26d05c59beb17514197d727de1be37be23a4
gabrieljdantas <gabrieljdantas26@gmail.com>
2023-07-06 11:54:12 -0300

    Aula 060723 - repeticoes

---
+++ aula-060723/repeticoes.js
@@ -0,0 +1,39 @@
+
+
+//Incrementar valor em uma unidade
+console.log("===============================");
+for (let numero = 0; numero < 10; numero++) {
+    console.log(numero);
+    
+}
+
+//Incrementar valor em duas unidade
+console.log("===============================");
+for (let numero = 0; numero < 10; numero += 2) {
+    console.log(numero);
+    
+}
+
+//Decrementar valor em uma unidade
+console.log("===============================");
+for (let numero = 10; numero >= 0; numero--) {
+    console.log(numero);
+    
+}
+
+console.log("===============================");
+for (let numero = 0; numero < 10;) {
+    numero++;
+    console.log(numero);
+    
+}
+console.log("===============================");
+let n = 0;
+while(n < 17) {
+    console.log(n+ " SoulCode- JavaScript");
+    n++;
+}
+
+//For tem uma quantidade de repetições pre determinadas, e o while tem uma quantidade de repetições indertemidas
+
+
