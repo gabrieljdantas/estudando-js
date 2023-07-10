2272d52f786220e4e682d7aa258730f7a7c87592
gabrieljdantas <gabrieljdantas26@gmail.com>
2023-07-10 18:09:20 -0300

    funçoes em js

---
+++ aula-100723/funcoes-js.js
@@ -0,0 +1,45 @@
+//Função ou Function -> Rotina
+
+// let num = 16;
+// let quadrado = num**2;
+// console.log(quadrado);
+
+// let result = console.log("OI")
+// console.log(result)
+// // ...
+
+
+//Funções de texto
+
+const texto = "Java Script ";
+
+console.log(texto.charAt(0));
+
+console.log(texto.concat(" bata", "ta"));
+
+console.log(texto.slice(-1));
+console.log(texto.trim())
+
+console.log(texto.replaceAll(" ", ""))
+
+console.log(texto.includes("Java"))
+console.log()
+
+console.log(texto.toUpperCase())
+
+console.log(texto.indexOf("y"))
+console.log(texto.substring(0, 4))
+
+
+// Math -> Biblioteca Matematica
+
+console.log(Math.ceil(2.3546));
+console.log(Math.floor(2.3546))
+//arredonda automatico
+console.log(Math.round(2.3546))
+//Raiz quadrada
+console.log(Math.sqrt(625))
+//Modulo de um numero
+console.log(Math.abs(-8))
+
+
