4972dd2eec5165f4d1f52b02fe231eb973d82a55
gabrieljdantas <gabrieljdantas26@gmail.com>
2023-07-18 11:31:42 -0300

    aula18-07-23 - JS com html

---
+++ aula-180723/script.js
@@ -0,0 +1,24 @@
+// window.document
+
+let h1 = document.getElementById("titulo");
+console.log(h1)
+
+let p = document.getElementsByClassName("paragrafo");
+
+let b = document.getElementsByTagName("b");
+
+let campoEmail = document.querySelector("input[type=email]");
+
+let negritoParagrafo = document.querySelector("p b")
+let negritosParagrafos = document.querySelectorAll("p b")
+let paragrafo = document.querySelector("p")
+
+// h1.title = "title"
+// h1.style.color = "blue"
+// h1.style.fontSize = "30px"
+
+console.log(campoEmail)
+console.log(paragrafo.innerText)
+paragrafo.innerHTML = "Hoje na aula de JavaScript conversamos sobre DOM"
+console.log(paragrafo.innerHTML)
+console.log(negritosParagrafos)
